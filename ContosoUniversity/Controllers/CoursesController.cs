d8121aa653d7c0fad3d07e93d43b1baa17962ace
Siya Khumalo <107889389+siyakhumalo-ms@users.noreply.github.com>
2025-12-19 01:32:22 +0000

    Add upgrade docs; optimize CoursesController Index query
    Added upgrade tracking markdown files to project as non-source items.
    Updated CoursesController Index action to use AsNoTracking for
    better performance in read-only queries.

--- ContosoUniversity/Controllers/CoursesController.cs
+++ ContosoUniversity/Controllers/CoursesController.cs
@@ -24,8 +24,11 @@ namespace ContosoUniversity.Controllers
         // GET: Courses
         public ActionResult Index()
         {
-            var courses = db.Courses.Include(c => c.Department);
-            return View(courses.ToList());
+            var courses = db.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToList();
+            return View(courses);
         }
 
         // GET: Courses/Details/5
